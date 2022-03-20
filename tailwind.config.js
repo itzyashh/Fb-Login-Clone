34038a22f2fd6c67227098eb02596c9b4cc97c1a
itzyashh <82391577+itzyashh@users.noreply.github.com>
2022-03-20 11:05:49 +0530

    with gitignore added

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+   
+    extend: { colors: {
+      fbBg: '#f0f2f5',
+      fbBtnLogin: '#1877f2',
+      borderbClr: '#dadde1',
+      fbGreenclr:'#42b72a',
+      
+    },
+  },
+  },
+  plugins: [],
+}
